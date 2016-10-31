7983539ee6a8bc6ec6f529232d5f15640771cf88
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-10-31 10:43:53 +0100

    Fix associations where name equals foreign key name

--- lib/active_musicbrainz/models/model.rb
+++ lib/active_musicbrainz/models/model.rb
@@ -9,10 +9,14 @@ module ActiveMusicbrainz
       true
     end
 
+    ##
+    # Please note that the association name cannot match the foreign key name
+    # https://github.com/rails/rails/issues/26778#issuecomment-255592061
+    #
     Factory.define do
       model :artist do
-        has_many    :artist_credit_names, foreign_key: :artist
-        has_many    :artist_credits, through: :artist_credit_name
+        has_many    :credit_names, foreign_key: :artist, class_name: 'ArtistCreditName'
+        has_many    :credits, through: :artist_credit_name, class_name: 'ArtistCredit'
         has_many    :release_groups, through: :artist_credits
         has_many    :releases, through: :release_groups
         has_many    :recordings, through: :artist_credits
@@ -27,17 +31,17 @@ module ActiveMusicbrainz
       end
 
       model :l_artist_url do
-        belongs_to :artist, foreign_key: :entity0
-        belongs_to :url, foreign_key: :entity1
+        belongs_to  :artist, foreign_key: :entity0
+        belongs_to  :url, foreign_key: :entity1
       end
 
       model :artist_credit_name do
-        belongs_to  :artist_credit, foreign_key: :artist_credit
-        belongs_to  :artist, foreign_key: :artist
+        belongs_to  :credit, foreign_key: :artist_credit, class_name: 'ArtistCredit'
+        belongs_to  :credit_artist, foreign_key: :artist
       end
 
       model :artist_credit do
-        has_many    :artist_credit_names, foreign_key: :artist_credit
+        has_many    :credit_names, foreign_key: :artist_credit, class_name: 'ArtistCreditName'
         has_many    :artists, through: :artist_credit_names
         has_many    :recordings, foreign_key: :artist_credit
         has_many    :tracks, foreign_key: :artist_credit
@@ -46,7 +50,12 @@ module ActiveMusicbrainz
       end
 
       model :artist_alias do
-        belongs_to  :artist, foreign_key: :artist
+        belongs_to :alias_artist, foreign_key: :artist, class_name: 'Artist'
+        belongs_to :alias_type, foreign_key: :type, class_name: 'ArtistAliasType'
+      end
+
+      model :artist_alias_type do
+        has_many    :aliases, foreign_key: :type, class_name: 'ArtistAlias'
       end
 
       model :artist_type do
@@ -59,7 +68,7 @@ module ActiveMusicbrainz
 
       model :area do
         has_many    :artists, foreign_key: :area
-        belongs_to  :type, foreign_key: :type, class_name: 'AreaType', optional: true
+        belongs_to  :area_type, foreign_key: :type, class_name: 'AreaType', optional: true
       end
 
       model :area_type do
@@ -72,28 +81,28 @@ module ActiveMusicbrainz
       end
 
       model :track do
-        belongs_to  :recording, foreign_key: :recording
-        belongs_to  :medium, foreign_key: :medium
+        belongs_to  :track_recording, foreign_key: :recording
+        belongs_to  :track_medium, foreign_key: :medium
         has_one     :release, through: :medium
         has_artist_credits
         has_gid
       end
 
       model :release_group_secondary_type do
-        has_many :release_group_secondary_type_joins, foreign_key: :secondary_type
-        has_many :release_groups, through: :release_group_secondary_type_joins
+        has_many    :release_group_secondary_type_joins, foreign_key: :secondary_type
+        has_many    :release_groups, through: :release_group_secondary_type_joins
       end
 
       model :release_group_secondary_type_join do
-        belongs_to :release_group, foreign_key: :release_group
-        belongs_to :type, class_name: 'ReleaseGroupSecondaryType', foreign_key: :secondary_type
+        belongs_to  :release_group_secondary_type_join_release_group, foreign_key: :release_group
+        belongs_to  :type, class_name: 'ReleaseGroupSecondaryType', foreign_key: :secondary_type
       end
 
       model :release_group do
         has_many    :releases, foreign_key: :release_group
         has_many    :recordings, through: :releases
         belongs_to  :meta, class_name: 'ReleaseGroupMeta', foreign_key: :id
-        belongs_to  :type, class_name: 'ReleaseGroupPrimaryType', foreign_key: :type
+        belongs_to  :release_group_type, class_name: 'ReleaseGroupPrimaryType', foreign_key: :type
         has_many    :release_group_secondary_type_joins, foreign_key: :release_group
         has_many    :secondary_types, through: :release_group_secondary_type_joins, source: :type
         has_artist_credits
@@ -101,13 +110,13 @@ module ActiveMusicbrainz
       end
 
       model :release do
-        belongs_to  :release_group, foreign_key: :release_group
+        belongs_to  :release_release_group, foreign_key: :release_group
         belongs_to  :meta, class_name: 'ReleaseMeta', foreign_key: :id
         has_many    :mediums, -> { order('medium.position') }, foreign_key: :release
         has_many    :recordings, through: :mediums
         has_many    :tracks, through: :mediums
-        belongs_to  :status, class_name: 'ReleaseStatus', foreign_key: :status
-        belongs_to  :packaging, class_name: 'ReleasePackaging', foreign_key: :packaging
+        belongs_to  :release_status, class_name: 'ReleaseStatus', foreign_key: :status
+        belongs_to  :release_packaging, class_name: 'ReleasePackaging', foreign_key: :packaging
         has_many    :release_countries, foreign_key: :release
 
         has_artist_credits
@@ -115,10 +124,10 @@ module ActiveMusicbrainz
       end
 
       model :medium do
-        belongs_to  :release, foreign_key: :release
+        belongs_to  :medium_release, foreign_key: :release
         has_many    :tracks, -> { order('track.position') }, foreign_key: :medium
         has_many    :recordings, through: :tracks
-        belongs_to  :format, class_name: 'MediumFormat', foreign_key: :format
+        belongs_to  :medium_format, class_name: 'MediumFormat', foreign_key: :format
       end
 
       model :recording do
